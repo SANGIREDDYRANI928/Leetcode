5b1acd2f5c531ee5ddd7c98b0f57e1324a8dc0d3
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-07-25 17:02:32 +0530

    Time: 507 ms (52.57%), Space: 29.9 MB (79.1%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, nums, k):
+        nums.sort()
+        p1=0
+        p2=len(nums)-1
+        counter=0
+        while(p1<p2):
+            if nums[p1]+nums[p2]==k:
+                counter+=1
+                p1+=1
+                p2-=1
+            elif nums[p1]+nums[p2]>k:
+                p2-=1
+            else:
+                p1+=1
+        return counter
+
+        
