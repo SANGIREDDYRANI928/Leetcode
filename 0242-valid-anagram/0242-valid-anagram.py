a13c34340e442cfd3fe24e4cb4bfa9e19946d713
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-07-28 20:17:51 +0530

    Time: 11 ms (72.15%), Space: 18 MB (49.64%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s, t):
+        dict1=dict()
+        dict2=dict()
+        for i in s:
+            if i in dict1:
+                dict1[i]+=1
+            else:
+                dict1[i]=1
+        for i in t:
+            if i in dict2:
+                dict2[i]+=1
+            else:
+                dict2[i]=1
+        if dict1==dict2:
+            return True
+        else:
+            return False
+
+
+        
