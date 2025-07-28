15e6892f2219057c6ebeb96b95bb8073b318ff83
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-07-28 20:46:42 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.93%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num):
+        while(num>=10):
+            sum=0
+            while(num>0):
+                sum+=num%10
+                num=num//10
+            num=sum
+        return num
+
+        
