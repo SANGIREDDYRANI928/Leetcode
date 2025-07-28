49c8af3879de039382ac0ba000ed67b9fc682b07
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-07-28 20:09:30 +0530

    Time: 15 ms (54.19%), Space: 31.7 MB (20.34%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums):
+
+        set1=set()
+        for i in nums:
+            if i in set1:
+                return True
+            else:
+                set1.add(i)
+        return False
