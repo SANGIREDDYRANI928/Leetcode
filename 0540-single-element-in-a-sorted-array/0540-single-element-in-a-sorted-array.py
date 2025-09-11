7b1d07b8627689aec4a3a9e65f170d4080334b25
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-09-11 19:47:18 +0530

    Time: 0 ms (100%), Space: 24.8 MB (45.17%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         l, h = 0, len(nums) - 1
         while l < h:
             mid = (l + h) // 2
-            if mid % 2 == 1:  # force mid to be even
+            if mid % 2 == 1: 
                 mid -= 1
             if nums[mid] == nums[mid + 1]:
                 l = mid + 2
