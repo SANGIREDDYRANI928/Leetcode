0cf3325dd528da3fd5ea3f48c21da03f2342f7df
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2025-09-21 00:15:11 +0530

    Time: 7 ms (39.49%), Space: 18 MB (20.76%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+        
+        sum1 = 1 
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                sum1 += i
+                if i != num // i:
+                    sum1 += num // i
+            i += 1
+        
+        return sum1 == num
