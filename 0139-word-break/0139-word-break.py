96318169b0028a7917e77e8d84265643fc9cca3d
SANGI REDDY RANI <sangireddyrani32@gmail.com>
2026-09-21 00:39:56 +0530

    Time: 7 ms (58.35%), Space: 12.5 MB (16.99%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        n=len(s)
+        wordSet=set(wordDict)
+        dp=[False]*(n+1)
+        dp[0]=True
+        for i in range(1,n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i]=True
+                    break
+        return dp[n]
